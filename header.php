14a4c1610eb29579b1ec028c4dcf6856c4ef6e9c
Krishna sis Tripathy <108582463+siskrishna@users.noreply.github.com>
2023-01-18 19:28:00 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+    <!-- navbar part start -->
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+       <a href="#"><img src="logo.png" class="logo"></a>
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Knox Bank</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="customer.php">View all Customers</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+        <!-- navbar part ended -->
